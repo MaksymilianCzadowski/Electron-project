e19105073ed38ac7ed3e46a8b1ab4fd8db40c0ef
czadowski <maksymilian.czadowski@gmail.com>
2023-04-02 19:46:23 +0200

    sound notification

--- public/preload.js
+++ public/preload.js
@@ -1,6 +1,7 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 const { contextBridge } = require("electron");
+const sound = require("sound-play");
 
 // As an example, here we use the exposeInMainWorld API to expose the browsers
 // and node versions to the main window.
@@ -9,6 +10,7 @@ process.once("loaded", () => {
   contextBridge.exposeInMainWorld("versions", process.versions);
   contextBridge.exposeInMainWorld("electron", {
     alertNotification: (sender, content) => {
+      sound.play("./public/sounds/discord-notification.mp3");
       new Notification(`You just receive new message from ${sender}`, {
         body: content,
       });
